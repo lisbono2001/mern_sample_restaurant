52c41531f4d4b5ffde10eed793c978b495c44cc6
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2022-01-30 02:33:17 +0700

    Initial Commit

---
+++ frontend/src/components/add-review.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link, useParams, useLocation } from "react-router-dom";
+
+const AddReview = ({ user }) => {
+  const { id } = useParams();
+  const { state } = useLocation();
+
+  let initialReviewState = "";
+
+  let editing = false;
+
+  if (state && state.currentReview) {
+    editing = true;
+    initialReviewState = state.currentReview.text;
+  }
+
+  const [review, setReview] = useState(initialReviewState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    setReview(event.target.value);
+  };
+
+  const saveReview = () => {
+    var data = {
+      text: review,
+      name: user.name,
+      user_id: user.id,
+      restaurant_id: id,
+    };
+    console.log(editing);
+    if (editing) {
+      data.review_id = state.currentReview._id;
+      RestaurantDataService.updateReview(data)
+        .then((response) => {
+          setSubmitted(true);
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      RestaurantDataService.createReview(data)
+        .then((response) => {
+          setSubmitted(true);
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div className="submit-form">
+          {submitted ? (
+            <div>
+              <h4>You submitted successfully!</h4>
+              <Link to={"/restaurants/" + id} className="btn btn-success">
+                Back to Restaurant
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <div className="form-group">
+                <label htmlFor="description">
+                  {editing ? "Edit" : "Create"} Review
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="text"
+                  required
+                  value={review}
+                  onChange={handleInputChange}
+                  name="text"
+                />
+              </div>
+              <button onClick={saveReview} className="btn btn-success">
+                Submit
+              </button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>Please log in.</div>
+      )}
+    </div>
+  );
+};
+
+export default AddReview;
