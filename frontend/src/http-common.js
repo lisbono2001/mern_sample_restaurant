70585d9a65c07ec35fcb878f786359f840eb2187
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2022-01-30 22:17:58 +0700

    Config paths for MongoDB Realm deployment

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL:
+    "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-mixns/service/Cluster0/incoming_webhook/",
   headers: {
     "Content-type": "application/json",
   },
